d06a7979f8132c7b5ab9e92619ecf819ef89f415
mingkang <mingkang@student.42seoul.kr>
2025-12-12 10:26:45 +0900

    feat: Add splash screen
    Add the `SplashScreen` API dependency and integrate it into `MainActivity`.
    Create a `SplashViewModel` to handle pre-fetching of coin data and manage the splash screen's visibility.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.navigation3)
 
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.core.splashscreen)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
--- app/src/main/java/io/soma/cryptobook/MainActivity.kt
+++ app/src/main/java/io/soma/cryptobook/MainActivity.kt
@@ -4,42 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.activity.viewModels
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
-import io.soma.cryptobook.navigation.CbNavigator
-import io.soma.cryptobook.navigation.rememberCbNavigator
-import io.soma.cryptobook.ui.theme.CryptoBookTheme
+import io.soma.cryptobook.splash.SplashViewModel
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val viewModel: SplashViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        val splashScreen = installSplashScreen()
+
+        splashScreen.setKeepOnScreenCondition {
+            !viewModel.isReady.value
+        }
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
             CryptoBookApp()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CryptoBookTheme {
-        Greeting("Android")
     }
 }
