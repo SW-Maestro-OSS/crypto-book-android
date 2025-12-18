3b703f071ad7d5fd90f9e7d8ee5ce43b4d3779f4
mingkang <mingkang@student.42seoul.kr>
2025-12-18 16:04:19 +0900

    feat(coin-list): Fetch 24hr ticker data to show price changes
    Updates the Binance API service to fetch data from the `/api/v3/ticker/24hr` endpoint instead of `/api/v3/ticker/price`.

--- coin-list/data/src/main/java/io/soma/cryptobook/coinlist/data/network/BinanceApiService.kt
+++ coin-list/data/src/main/java/io/soma/cryptobook/coinlist/data/network/BinanceApiService.kt
@@ -5,6 +5,6 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface BinanceApiService {
-    @GET("api/v3/ticker/price")
+    @GET("api/v3/ticker/24hr")
     suspend fun getAllTickerPrices(): Response<List<BinanceTickerDto>>
 }
