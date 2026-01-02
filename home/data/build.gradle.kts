56f9d1a7e2cf22360bf03bde6d095beffa9e48bc
mingkang <mingkang@student.42seoul.kr>
2026-01-02 13:29:06 +0900

    chore: Remove unused defaultConfig and buildTypes from build.gradle.kts

--- home/data/build.gradle.kts
+++ home/data/build.gradle.kts
@@ -7,21 +7,6 @@ plugins {
 
 android {
     namespace = "io.soma.cryptobook.home.data"
-
-    defaultConfig {
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro",
-            )
-        }
-    }
 }
 
 dependencies {
